555211fb8d6fdcf40f9f5ea602534f4ea506938e
unknown <you@example.com>
2020-06-10 12:54:11 +0530

    Emp Present or Absent

---
+++ EmpPresentOrAbsent.java
@@ -0,0 +1,11 @@
+class EmpPresentOrAbsent{
+     public static void main(String[] args){
+        int A = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+         if  (empCheck == A )
+              System.out.println("emp is present");
+         else
+              System.out.println("emp is absent");
+       }
+}
+
